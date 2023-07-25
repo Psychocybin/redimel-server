0f8dfc4e9183eaa010b486ee2a1f6a11a944230e
GeorgiGeorgiev <boyanov81gg@gmail.com>
2023-07-25 11:50:21 +0300

    CreatingAuthDatabase with two roles

--- Data/RedimelServerAuthDbContext.cs
+++ Data/RedimelServerAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace redimel_server.Data
@@ -7,7 +8,34 @@ namespace redimel_server.Data
     {
         public RedimelServerAuthDbContext(DbContextOptions<RedimelServerAuthDbContext> options) : base(options)
         {
+        }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "89076e89-a13a-496a-96fd-8696e9539f78";
+            var writerRoleId = "6b9e8bf5-6436-4d04-accb-b02c7b573c95";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
