ac75f9e4f1da87833141e56b2742efe5c880b0c7
GeorgiGeorgiev <boyanov81gg@gmail.com>
2023-09-08 14:54:40 +0300

    adding additional abilities

--- Data/RedimelServerAuthDbContext.cs
+++ Data/RedimelServerAuthDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using redimel_server.Repositories;
 
 namespace redimel_server.Data
 {
